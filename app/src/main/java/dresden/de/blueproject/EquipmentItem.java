3ad77c3653f99812a1daaf7eb82ee00e42082b12
firesoft_de <firestorm1331@live.de>
2017-12-20 17:09:30 +0100

    Layout für list_item erstellt. Datenklassen vorbereitet

--- app/src/main/java/dresden/de/blueproject/EquipmentItem.java
+++ app/src/main/java/dresden/de/blueproject/EquipmentItem.java
@@ -1,10 +1,44 @@
 package dresden.de.blueproject;
 
 
+import android.media.Image;
+
 /**
- * Diese Klasse implementiert die Datenstruktur für einen einzelnen Ausstattungsgegenstand
+ * {@link EquipmentItem} Diese Klasse implementiert die Datenstruktur für einen einzelnen Ausstattungsgegenstand
  */
 public class EquipmentItem {
     //TODO Implementieren
+
+    //Interne Variablen
+
+    String mName;
+
+    String mDescription;
+
+    int mID;
+
+    //Enthält ein Bild des Items
+    Image mImage;
+
+    //Verzeichnet die Position des Items
+    String mPosition;
+
+    /**
+     * Datenklasse initialiseren
+     * @param id ID des Items
+     * @param name Name des Items
+     * @param description Beschreibung zum Item
+     */
+    public void EquitmentItem(int id, String name, String description, String position) {
+
+        mID = id;
+        mName = name;
+        mDescription = description;
+        mPosition = position;
+
+    }
+
+
+
 
 }
--- app/src/main/java/dresden/de/blueproject/MainActivity.java
+++ app/src/main/java/dresden/de/blueproject/MainActivity.java
@@ -5,15 +5,25 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<TrayItem> trays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Test um ein anderes Fragment darzustellen
         FragmentTransaction ft = this.getSupportFragmentManager().beginTransaction();
         Fragment beladungFragment = new TrayFragment();
         ft.replace(R.id.MainFrame, beladungFragment);
         ft.commit();
+
+
+
     }
 }
