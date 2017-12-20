d3e01259d673bf29a208a3fedb530e946d9553d5
firesoft_de <firestorm1331@live.de>
2017-12-20 17:13:14 +0100

    Adapter stub

---
+++ app/src/main/java/dresden/de/blueproject/ItemAdapter.java
@@ -0,0 +1,23 @@
+package dresden.de.blueproject;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class ItemAdapter extends ArrayAdapter<EquipmentItem> {
+
+    public ItemAdapter(Activity context, ArrayList<EquipmentItem> earthquakes) {
+        super(context,0,earthquakes);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
