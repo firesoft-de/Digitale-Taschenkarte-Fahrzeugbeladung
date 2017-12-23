d699f2dffcce74c86d49300ab1e67acd3da394c3
firesoft-de <34716031+firesoft-de@users.noreply.github.com>
2017-12-23 23:23:09 +0100

    - Suchfunktion eingebaut - Leichte Design Anpassung - Datenstruktur geändert um Suche zu ermöglichen - EquipmentItem ist jetzt Parcelable

---
+++ app/src/main/java/dresden/de/blueproject/SearchableActivity.java
@@ -0,0 +1,93 @@
+package dresden.de.blueproject;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG="SearchableActivity_LOG";
+
+    ArrayList<EquipmentItem> equipmentItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+
+        //Search-Intent abrufen und auswerten
+        Intent intent = getIntent();
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+
+            Bundle bundle = intent.getExtras();
+
+            try {
+                equipmentItems =  bundle.getParcelableArrayList("EL");
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+                Log.e(LOG_TAG, "Fehler beim Abrufen des Bundles in der SearchActivity! Cause: "+e.getCause() + " | Nachricht: " + e.getMessage());
+            }
+
+            String searchQuery = intent.getStringExtra(SearchManager.QUERY);
+            ArrayList<EquipmentItem> res =  search(searchQuery);
+
+            presentData(res);
+
+        }
+
+    }
+
+    private ArrayList<EquipmentItem> search(String query) {
+
+        //Suchanfrange aufbereiten
+        String[] searchArray = (query.toLowerCase()).split(" ");
+
+        ArrayList<EquipmentItem> resultList = new ArrayList<>();
+
+        for (int count = 0; count <equipmentItems.size(); count++) {
+
+            EquipmentItem item = equipmentItems.get(count);
+
+            //Suche mittels Keywords
+
+            for (int i = 0; i < searchArray.length; i++) {
+
+                String[] keywords = item.getKeywords();
+
+                for (int x = 0; x < keywords.length; x++) {
+                    if (keywords[x].contains(searchArray[i]) ) {
+                        resultList.add(item);
+                        i = searchArray.length;
+                        x = keywords.length;
+                    }
+                }
+            }
+        }
+
+        return resultList;
+
+    }
+
+    private void presentData(ArrayList<EquipmentItem> searchResults) {
+
+        ItemAdapter searchAdapter = new ItemAdapter(this, searchResults);
+
+        ListView lv = (ListView) this.findViewById(R.id.ListViewSearch);
+
+        lv.setAdapter(searchAdapter);
+
+        searchAdapter.notifyDataSetChanged();
+
+    }
+
+}
