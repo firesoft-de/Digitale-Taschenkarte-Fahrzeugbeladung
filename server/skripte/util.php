e3caa4ac6a53d135ea2ad2b0aadfafb874444431
firesoft-de <34716031+firesoft-de@users.noreply.github.com>
2018-01-24 17:04:41 +0100

    Überarbeitung Skripte
    - Datenbankskript aufgeräumt
    - Logfunktion für die Datenbank eingebaut

--- server/skripte/util.php
+++ server/skripte/util.php
@@ -113,6 +113,74 @@
 		}
 				
 		return $idarray;		
-	}		
+	}
+	
+	function log_db($message) {
+		$dbFile = fopen("config/log_db.txt",'a');
+		
+		fwrite($dbFile,"Timestamp: ");
+		fwrite($dbFile,date("Y-m-d H:i:s"));
+		fwrite($dbFile,"; Message: ");
+		fwrite($dbFile,$message);
+		fwrite($dbFile,"\r\n");
+		
+		fclose($dbFile);
+	}
+	
+	function getDBVersion() {
+		$dbFile = fopen("db_Version.txt",'r');
+		$dbversion = fgets($dbFile);
+		fclose($dbFile);
+		return $dbversion;
+	}
+	
+	function getDBAccess() {
+		
+		global $db_server;
+		global $db_name;
+		global $db_user;
+		global $db_password;
+		
+		//Datenbankzugangsdaten
+		$dbFile = fopen(__DIR__ .  "/config/access.txt",'r');
+		
+		$db_server = fgets($dbFile);	
+		$db_name = fgets($dbFile);	
+		$db_user = fgets($dbFile);
+		$db_password = fgets($dbFile);
+		
+		fclose($dbFile);
+		 
+		$db_server = trim(preg_replace('/\s+/', ' ', $db_server));
+		$db_name = trim(preg_replace('/\s+/', ' ', $db_name));
+		$db_user = trim(preg_replace('/\s+/', ' ', $db_user));
+		$db_password = trim(preg_replace('/\s+/', ' ', $db_password));
+	}
+	
+	function debugJSON() {
+		switch (json_last_error()) {
+			case JSON_ERROR_NONE:
+				echo ' - No errors';
+				break;
+			case JSON_ERROR_DEPTH:
+				echo ' - Maximum stack depth exceeded';
+				break;
+			case JSON_ERROR_STATE_MISMATCH:
+				echo ' - Underflow or the modes mismatch';
+				break;
+			case JSON_ERROR_CTRL_CHAR:
+				echo ' - Unexpected control character found';
+				break;
+			case JSON_ERROR_SYNTAX:
+				echo ' - Syntax error, malformed JSON';
+				break;
+			case JSON_ERROR_UTF8:
+				echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+				break;
+			default:
+				echo ' - Unknown error';
+				break;
+		}		
+	}
 
 ?>
