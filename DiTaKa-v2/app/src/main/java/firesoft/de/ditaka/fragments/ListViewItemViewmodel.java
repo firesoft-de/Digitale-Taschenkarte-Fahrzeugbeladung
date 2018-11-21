8a476da67c2b6127849fbcbd273e414760882856
firesoft_de <firestorm1331@live.de>
2018-11-21 19:01:49 +0100

    ListViewItem erstellt

---
+++ DiTaKa-v2/app/src/main/java/firesoft/de/ditaka/fragments/ListViewItemViewmodel.java
@@ -0,0 +1,74 @@
+/*
+ *     Diese App stellt die Beladung von BOS Fahrzeugen in digitaler Form dar.
+ *     Copyright (C) 2018  David Schlossarczyk
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     For the full license visit https://www.gnu.org/licenses/gpl-3.0.
+ */
+
+package firesoft.de.ditaka.fragments;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import firesoft.de.ditaka.datamodels.BaseDataClass;
+import firesoft.de.ditaka.datamodels.Item;
+import firesoft.de.ditaka.datamodels.Tray;
+
+/**
+ * Viewmodel für die angepassten ListViewItems.
+ */
+public class ListViewItemViewmodel extends ViewModel {
+
+    // region UI-Livedata
+
+    public MutableLiveData<String> title;
+    public MutableLiveData<String> location;
+
+    // endregion
+
+    // region Datenvariablen
+
+    /**
+     * Die Daten werden als BaseDataClass bereitgestellt, da von dieser Klasse alle anzuzeigenden Datentypen abgeleitet werden.
+     */
+    BaseDataClass data;
+
+    // endregion
+
+    /**
+     * Erstellt ein neues Viewmodel für ein ListViewItem. Für jedes Item wird ein eigenes Viewmodel benötigt.
+     * @param data
+     */
+    public ListViewItemViewmodel(BaseDataClass data) {
+
+        this.data = data;
+
+        title = new MutableLiveData<>();
+        location = new MutableLiveData<>();
+
+        title.postValue(data.getName());
+
+        // Falls ein Item oder ein Tray übergeben wurde, die Position abfragen.
+        // Es sollte eigentlich immer ein Tray oder ein Item übergeben werden. Das ganze ist daher eher eine Sicherheitsmaßnahme
+        if (data instanceof Item ) {
+            location.postValue(((Item) data).getLocation());
+        }
+        else if (data instanceof Tray) {
+            location.postValue(((Tray) data).getLocation());
+        }
+
+
+    }
+
+
+
+}
