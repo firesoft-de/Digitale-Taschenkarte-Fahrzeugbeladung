547def59f4d226b237e74f1c81997acb7516dbb6
firesoft_de <firestorm1331@live.de>
2018-11-21 19:02:22 +0100

    TrayFragment-Stub erstellt

---
+++ DiTaKa-v2/app/src/main/java/firesoft/de/ditaka/fragments/trayFragment/TrayFragment.java
@@ -0,0 +1,47 @@
+/*
+ *     Diese App stellt die Beladung von BOS Fahrzeugen in digitaler Form dar.
+ *     Copyright (C) 2018  David Schlossarczyk
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     For the full license visit https://www.gnu.org/licenses/gpl-3.0.
+ */
+
+package firesoft.de.ditaka.fragments.trayFragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import firesoft.de.ditaka.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TrayFragment extends Fragment {
+
+
+    public TrayFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tray, container, false);
+    }
+
+}
